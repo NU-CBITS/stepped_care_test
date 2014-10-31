95303e60ce7b82145c50bd091ee52c9d7e48cdbc
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-10-31 17:37:43 -0500

    batch file to run each environment

---
+++ tests.rb
@@ -0,0 +1,3 @@
+system("rspec -r./SC_configure_cloud_saucelabs_XP_chrome_32.rb")
+system("rspec -r./SC_configure_cloud_saucelabs_XP_firefox_27.rb")
+system("rspec -r./SC_configure_cloud_saucelabs_XP_ie_8.rb")
