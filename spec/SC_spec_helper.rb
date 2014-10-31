89d6fe431b51779f3be7be9fc0a6af06ec437b6d
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-10-31 16:47:37 -0500

    add browsers

--- spec/SC_spec_helper.rb
+++ spec/SC_spec_helper.rb
@@ -28,6 +28,17 @@ RSpec.configure { |config|
            url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
            desired_capabilities: caps)
 
+     when 'saucelabs_XP_chrome_32'
+       caps = Selenium::WebDriver::Remote::Capabilities.send(ENV['browser'])
+       caps.version = ENV['browser_version']
+       caps.platform = ENV['operating_system']
+       caps[:name] = RSpec.current_example.metadata[:full_description]
+
+       @driver = Selenium::WebDriver.for(
+           :remote,
+           url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
+           desired_capabilities: caps)
+
    else
       @driver = Selenium::WebDriver.for :firefox
 
