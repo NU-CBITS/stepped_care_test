1b7ed763e314834a68206c2ccab20d3d6a86579f
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-11-04 15:43:00 -0600

    added sauce_whisk to insert pass/fail result in sauce labs job

--- SC_configure_cloud_saucelabs.rb
+++ SC_configure_cloud_saucelabs.rb
@@ -1,5 +1,7 @@
 # filename: SC_configure_cloud_saucelabs.rb
 
+require 'sauce_whisk'
+
 ENV['SAUCE_USERNAME']   ||= 'Your Sauce Username'
 ENV['SAUCE_ACCESS_KEY'] ||= 'Your Sauce Access Key'
 ENV['Participant_Email'] ||= 'SC Participant Email'
--- spec/SC_spec_helper.rb
+++ spec/SC_spec_helper.rb
@@ -4,6 +4,10 @@ require 'selenium-webdriver'
 
 RSpec.configure { |config|
 
+  def host_name_list
+    ['saucelabs_XP_ie_8', 'saucelabs_XP_firefox_32', 'saucelabs_XP_chrome_37', 'saucelabs_win7_ie_10','saucelabs_win7_firefox_32', 'saucelabs_win7_chrome_37', 'saucelabs_osx_106_safari_5', 'saucelabs_osx_106_firefox_32', 'saucelabs_osx_106_chrome_37', 'saucelabs_osx_108_safari_6', 'saucelabs_osx_108_chrome_37', 'saucelabs_osx_109_safari_7', 'saucelabs_osx_109_firefox_32', 'saucelabs_osx_109_chrome_37', 'saucelabs_osx_1010_firefox_32', 'saucelabs_osx_1010_chrome_37']
+  end
+
   config.before(:each) {
     @driver=nil
     case ENV['host']
@@ -192,9 +196,15 @@ RSpec.configure { |config|
     @driver.manage.timeouts.implicit_wait = 10
   }
 
-  config.after(:each) {
+  config.after(:each) do |example|
+    if host_name_list.include?(ENV['host'])
+      if example.exception.nil?
+        SauceWhisk::Jobs.pass_job @driver.session_id
+      else
+        SauceWhisk::Jobs.fail_job @driver.session_id
+      end
+    end
     @driver.quit
-  }
+  end
 
 }
-
