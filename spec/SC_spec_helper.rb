c067f6ada3ac6feea77e20f16c58a8eea4bd7107
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-11-09 15:48:30 -0600

    got the capybara test to run locally now trying to add in sauce configuration

--- spec/SC_spec_helper.rb
+++ spec/SC_spec_helper.rb
@@ -1,15 +1,53 @@
 # filename: SC_spec_helper.rb
 
 require 'capybara'
+require 'sauce'
+require 'rails'
+require 'sauce/capybara'
 require 'capybara/rspec'
+require 'capybara/rails'
 require 'selenium-webdriver'
 require 'sauce_whisk'
 
-RSpec.configure { |config|
+Capybara.default_driver = :sauce
+
+Sauce.config do |config|
+
+  config[:start_tunnel] = false
 
   def host_name_list
     ['saucelabs_XP_ie_8', 'saucelabs_XP_firefox_32', 'saucelabs_XP_chrome_37', 'saucelabs_win7_ie_10','saucelabs_win7_firefox_32', 'saucelabs_win7_chrome_37', 'saucelabs_osx_106_safari_5', 'saucelabs_osx_106_firefox_32', 'saucelabs_osx_106_chrome_37', 'saucelabs_osx_108_safari_6', 'saucelabs_osx_108_chrome_37', 'saucelabs_osx_109_safari_7', 'saucelabs_osx_109_firefox_32', 'saucelabs_osx_109_chrome_37', 'saucelabs_osx_1010_firefox_32', 'saucelabs_osx_1010_chrome_37']
   end
+
+  config[:browsers] = [
+    [ENV['operating_system'], ENV['browser'], ENV['browser_version']]
+    # ["Windows XP", "Internet Explorer", "8"],
+    # ["Windows XP", "Firefox", "32"],
+    # ["Windows XP", "Chrome", "37"],
+    # ["Windows 7", "Internet Explorer", "10"],
+    # ["Windows 7", "Firefox", "32"],
+    # ["Windows 7", "Chrome", "37"],
+    # ["OS X 10.6", "Firefox", "32"],
+    # ["OS X 10.6", "Chrome", "37"],
+    # ["OS X 10.6", "Safari", "5"],
+    # ["OS X 10.6", "Chrome", "37"],
+    # ["OS X 10.8", "Safari", "6"],
+    # ["OS X 10.9", "Firefox", "32"],
+    # ["OS X 10.9", "Chrome", "37"],
+    # ["OS X 10.9", "Safari", "7"],
+    # ["OS X 10.10", "Firefox", "32"],
+    # ["OS X 10.10", "Chrome", "37"]
+  ]
+
+  # job = SauceWhisk::Jobs.fetch job_id
+  # job.build = "0.0.1"
+  # job.visibility = "public"
+  # job.tags = "new_user"
+  # job.name = "Login"
+  # # job.custom_data = {:executor => "jparth", :team_city_config => "standard_with_instrumentation"}
+  # job.passed = false
+  #
+  # job.save
 
   config.before(:each) {
     @driver=nil
@@ -20,7 +58,7 @@ RSpec.configure { |config|
         caps.platform = ENV['operating_system']
         caps[:name] = RSpec.current_example.metadata[:full_description]
 
-        @driver = Selenium::WebDriver.for(
+        driver = Selenium::WebDriver.for(
           :remote,
           url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
           desired_capabilities: caps)
@@ -210,4 +248,4 @@ RSpec.configure { |config|
     @driver.quit
   end
 
-}
+end
--- spec/features/SC_login_capy_spec.rb
+++ spec/features/SC_login_capy_spec.rb
@@ -6,34 +6,40 @@
 
 #likely need to update SC_spec_helper.rb SC_login.rb
 
-require_relative '../../SC_login'
+require_relative '../../spec/SC_spec_helper'
 
-describe "Login", :type => :feature do
-  before :each do
-    Login.create(:participant_email => ENV['Participant_Email'], :participant_password => ENV['Participant_Password'])
-  end
+describe "Login", :sauce => true do
+  # before(:each) do
+  #   Capybara.current_driver = :selenium
+  # end
+
+  # before :each do
+  #   Login.with(:participant_email => ENV['Participant_Email'], :participant_password => ENV['Participant_Password'])
+  # end
 
   it "Login succeeded" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
-    within("#session") do
+    within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
       fill_in 'participant_password', :with => ENV['Participant_Password']
     end
     click_button 'Sign in'
-    expect(page).to have_content 'Successfully signed in'
+    expect(page).to have_content 'Signed in successfully'
   end
 
-  let(:other_user) do
-    Login.create(:participant_email => 'asdf@test.com', :participant_password => 'asdf')
-  end
+  # let(:other_user) do
+  #   Login.create(:participant_email => 'asdf@test.com', :participant_password => 'asdf')
+  # end
 
   it "Login failed" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
-    within("#session") do
-      fill_in 'participant_email', :with => other_user.participant_email
-      fill_in 'participant_password', :with => other_user.participant_password
+    within("#new_participant") do
+      fill_in 'participant_email', :with => 'asdf@test.com'
+      fill_in 'participant_password', :with => 'asdf'
     end
     click_button 'Sign in'
     expect(page).to have_content 'Invalid email address or password'
   end
+
+
 end
