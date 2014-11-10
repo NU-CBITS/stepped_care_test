aca498ca81f586e52da6a27a9e954c6d7e74a720
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-11-10 15:08:13 -0600

    add tests for skip and navbar functionality

--- spec/features/SC_think_spec.rb
+++ spec/features/SC_think_spec.rb
@@ -16,7 +16,7 @@ describe "Think", :type => :feature, :sauce => false do
   end
 
 #tests
-  it "- identifying" do
+ it "- identifying" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
@@ -63,9 +63,9 @@ describe "Think", :type => :feature, :sauce => false do
     expect(page).to have_content 'Good work'
     click_on 'Continue'
     expect(page).to have_content 'Add a New Thought'
-  end
+ end
 
-  it "- patterns" do
+ it "- patterns" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
@@ -93,9 +93,9 @@ describe "Think", :type => :feature, :sauce => false do
     expect(page).to have_content 'Good work!'
     click_on 'Continue'
     expect(page).to have_content 'Add a New Thought'
-  end
+ end
 
-  it "- reshape" do
+ it "- reshape" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
@@ -138,7 +138,7 @@ describe "Think", :type => :feature, :sauce => false do
     expect(page).to have_content 'Thought saved'
  end
 
-  it "- add a new thought" do
+ it "- add a new thought" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
@@ -161,9 +161,9 @@ describe "Think", :type => :feature, :sauce => false do
     expect(page).to have_content 'Thought saved'
     click_on 'Continue'
     expect(page).to have_content 'Add a New Thought'
-  end
+ end
 
-  it "- check thoughts" do
+ it "- check thoughts" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
@@ -177,5 +177,65 @@ describe "Think", :type => :feature, :sauce => false do
     expect(page).to have_content 'Harmful Thoughts'
     # the below reference will need to change depending on the data on the server
     expect(page).to have_content 'Testing add a new thought'
+ end
+
+  it "- skip functionality" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
+    within("#new_participant") do
+      fill_in 'participant_email', :with => ENV['Participant_Email']
+      fill_in 'participant_password', :with => ENV['Participant_Password']
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+    click_link 'THINK'
+    click_link 'Home'
+    click_link '#1 Identifying'
+    expect(page).to have_content 'You are what you think...'
+    click_on 'Skip'
+    expect(page).to have_content 'Now, your turn...'
+    click_link 'THINK'
+    click_link 'Home'
+    click_link '#2 Patterns'
+    expect(page).to have_content 'Thinking Patterns'
+    click_on 'Skip'
+    expect(page).to have_content "Let's start by figuring out which thought patterns the harmful thoughts you
+    identified might match."
+    click_link 'THINK'
+    click_link 'Home'
+    click_link '#3 Reshape'
+    expect(page).to have_content 'Challenging Harmful Thoughts'
+    click_on 'Skip'
+    expect(page).to have_content 'You said you had the following unhelpful thoughts:'
+  end
+
+  it "- navbar functionality" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
+    within("#new_participant") do
+      fill_in 'participant_email', :with => ENV['Participant_Email']
+      fill_in 'participant_password', :with => ENV['Participant_Password']
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+    click_link 'THINK'
+    click_link 'THINK Landing'
+    expect(page).to have_content 'Add a New Thought'
+    click_link 'THINK'
+    click_link 'Home'
+    expect(page).to have_content 'Add a New Thought'
+    click_link 'THINK'
+    click_link '#1 Identifying'
+    expect(page).to have_content 'You are what you think...'
+    click_link 'THINK'
+    click_link '#2 Patterns'
+    expect(page).to have_content 'Thinking Patterns'
+    click_link 'THINK'
+    click_link '#3 Reshape'
+    expect(page).to have_content 'Challenging Harmful Thoughts'
+    click_link 'THINK'
+    click_link 'Add a New Thought'
+    expect(page).to have_content 'This thought is:'
+    click_link 'THINK'
+    click_link 'Thoughts'
+    expect(page).to have_content 'Harmful Thoughts'
   end
 end
