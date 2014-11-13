ff4a0649f417d5fcf82dfeb3d36d0b4ef08e229a
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-11-13 14:01:34 -0600

    added tests for redirecting to login if not already logged in, forgot password functionality, and watching introductory slideshow

--- spec/features/SC_login_spec.rb
+++ spec/features/SC_login_spec.rb
@@ -16,7 +16,7 @@ describe "Login", :type => :feature, :sauce => false do
   end
 
 #tests
-  it "succeeded" do
+  it "- success" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => ENV['Participant_Email']
@@ -26,7 +26,7 @@ describe "Login", :type => :feature, :sauce => false do
     expect(page).to have_content 'Signed in successfully'
   end
 
-  it "failed" do
+  it "- failure" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
     within("#new_participant") do
       fill_in 'participant_email', :with => 'asdf@test.com'
@@ -36,5 +36,40 @@ describe "Login", :type => :feature, :sauce => false do
     expect(page).to have_content 'Invalid email address or password'
   end
 
+  it "- not logged in, redirect" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/navigator/contexts/THINK'
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end
+
+  it "- not logged in, intro slideshow" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
+    click_on 'Introduction to ThinkFeelDo'
+    expect(page).to have_content 'Welcome to ThinkFeelDo'
+    click_on 'Continue'
+    expect(page).to have_content 'How to Maximize Your Benefit from ThinkFeelDo'
+    click_on 'Continue'
+    expect(page).to have_content 'What Should I Do When I Log in?'
+    click_on 'Continue'
+    expect(page).to have_content "How Do I Know What's New on the Site?"
+    click_on 'Continue'
+    expect(page).to have_content 'What Should I Expect?'
+    click_on 'Continue'
+    expect(page).to have_content 'What Might Get in the Way?'
+    click_on 'Continue'
+    expect(page).to have_content 'Get Started'
+    click_on 'Done'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  it "- forgot password" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/participants/sign_in'
+    click_on 'Forgot your password?'
+    expect(page).to have_content 'Forgot your password?'
+    within("#new_participant") do
+      fill_in 'participant_email', :with => ENV['Participant_Email']
+    end
+    click_button 'Send me reset password instructions'
+    expect(page).to have_content 'You will receive an email with instructions on how to reset your password in a few minutes.'
+  end
 
 end
