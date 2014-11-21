15ad8f09a0d9de44c104eb44ff933697b390ef55
Chris Brenner <christopher.brenner1@northwestern.edu>
2014-11-21 14:28:28 -0600

    remove .idea

--- SC_batch.rb
+++ SC_batch.rb
@@ -2,7 +2,7 @@
 
 #if running on Sauce Labs comment this block out
 puts "testing locally"
-system("rspec -r./SC_configure_cloud_saucelabs.rb")
+system("rspec -r./spec/features/SC_do_spec.rb")
 
 #if running locally comment this block out
 # puts "testing_on_saucelabs"
--- spec/features/SC_feel_spec.rb
+++ spec/features/SC_feel_spec.rb
@@ -1,4 +1,4 @@
-# #filename: SC_feel_spec,rb
+#filename: SC_feel_spec,rb
 
 #this file is to test the functionality of logging in, selecting the "LEARN" section,
 # and reading through the first lesson "Think, Feel, Do Your Way Out of Depression"
