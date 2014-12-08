14aa22f4f9f86879a504affe3151c6a5f725c611
Christopher Brenner <christopher.brenner1@northwestern.edu>
2014-12-08 12:26:41 -0600

    added test for editing a provider in both the modules spec and the bug spec since it is currently a bug - these are commented out at the moment as they are not completed

--- spec/features/user/bugs_spec.rb
+++ spec/features/user/bugs_spec.rb
@@ -38,4 +38,51 @@ describe "User Dashboard Bugs", :type => :feature, :sauce => false do
     click_on 'Test video 1'
     expect(page).to have_content 'List item 3'
   end
+
+  # #Testing bug where you receive error message when trying to edit a provider
+  # it "- editing a provider" do
+  #   visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
+  #   within("#new_user") do
+  #     fill_in 'user_email', :with => ENV['User_Email']
+  #     fill_in 'user_password', :with => ENV['User_Password']
+  #   end
+  #   click_button 'Sign in'
+  #   expect(page).to have_content 'Signed in successfully'
+  #   click_on 'fun'
+  #   expect(page).to have_content 'Manage Content'
+  #   expect(page).to have_content 'Click the group below for which you wish to moderate.'
+  #   click_on 'Manage Content'
+  #   expect(page).to have_content 'Content Dashboard'
+  #   click_on 'Modules'
+  #   expect(page).to have_content 'Content Modules'
+  #   if page.has_text?('DO Activities')
+  #     click_on 'DO Activities'
+  #     expect(page).to have_content 'Edit Module'
+  #     click_on 'activity index provider'
+  #     expect(page).to have_content 'Content Provider'
+  #     expect(page).to have_content 'activity index provider'
+  #     click_on 'Edit'
+  #
+  #   else
+  #     find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
+  #     if page.has_text?('DO Activities')
+  #       click_on 'DO Activities'
+  #       expect(page).to have_content 'Edit Module'
+  #       click_on 'activity index provider'
+  #       expect(page).to have_content 'Content Provider'
+  #       expect(page).to have_content 'activity index provider'
+  #       click_on 'Edit'
+  #
+  #     else
+  #       find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
+  #       click_on 'DO Activities'
+  #       expect(page).to have_content 'Edit Module'
+  #       click_on 'activity index provider'
+  #       expect(page).to have_content 'Content Provider'
+  #       expect(page).to have_content 'activity index provider'
+  #       click_on 'Edit'
+  #
+  #     end
+  #   end
+  # end
 end
--- spec/features/user/content_author_modules_spec.rb
+++ spec/features/user/content_author_modules_spec.rb
@@ -183,6 +183,53 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     expect(page).to have_content 'Slideshow: Home Introduction'
   end
 
+  # #Testing updating a provider
+  # it "- destroying a provider" do
+  #   visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
+  #   within("#new_user") do
+  #     fill_in 'user_email', :with => ENV['User_Email']
+  #     fill_in 'user_password', :with => ENV['User_Password']
+  #   end
+  #   click_button 'Sign in'
+  #   expect(page).to have_content 'Signed in successfully'
+  #   click_on 'fun'
+  #   expect(page).to have_content 'Manage Content'
+  #   expect(page).to have_content 'Click the group below for which you wish to moderate.'
+  #   click_on 'Manage Content'
+  #   expect(page).to have_content 'Content Dashboard'
+  #   click_on 'Modules'
+  #   expect(page).to have_content 'Content Modules'
+  #   if page.has_text?('DO Activities')
+  #     click_on 'DO Activities'
+  #     expect(page).to have_content 'Edit Module'
+  #     click_on 'activity index provider'
+  #     expect(page).to have_content 'Content Provider'
+  #     expect(page).to have_content 'activity index provider'
+  #     click_on 'Edit'
+  #
+  #   else
+  #     find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
+  #     if page.has_text?('DO Activities')
+  #       click_on 'DO Activities'
+  #       expect(page).to have_content 'Edit Module'
+  #       click_on 'activity index provider'
+  #       expect(page).to have_content 'Content Provider'
+  #       expect(page).to have_content 'activity index provider'
+  #       click_on 'Edit'
+  #
+  #     else
+  #       find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
+  #       click_on 'DO Activities'
+  #       expect(page).to have_content 'Edit Module'
+  #       click_on 'activity index provider'
+  #       expect(page).to have_content 'Content Provider'
+  #       expect(page).to have_content 'activity index provider'
+  #       click_on 'Edit'
+  #
+  #     end
+  #   end
+  # end
+
   #Testing destroying a provider
   it "- destroying a provider" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
