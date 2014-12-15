25d8cd7b6e7eacf13e3566a117ba76e42374c9cf
Christopher Brenner <christopher.brenner1@northwestern.edu>
2014-12-14 18:00:42 -0600

    update user bugs spec due to database cleaning

--- spec/features/user/bugs_spec.rb
+++ spec/features/user/bugs_spec.rb
@@ -1,6 +1,6 @@
 #filename: user/bugs_spec.rb
 
-#this is to test the users functionality on the researcher dashboard.
+#this is to test the users Arm 1ctionality on the researcher dashboard.
 
 require_relative '../../../spec/spec_helper'
 require_relative '../../../spec/configure_cloud_saucelabs'
@@ -25,13 +25,13 @@ describe "User Dashboard Bugs", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Lessons'
-    expect(page).to have_content 'Listing Lessons'
+    click_on 'Lesson Modules'
+    expect(page).to have_content 'Listing Lesson Moduels'
     click_on 'Testing adding/updating slides/lessons'
     expect(page).to have_content 'Test 1'
     expect(page).to have_content 'Test video 1'
@@ -39,50 +39,80 @@ describe "User Dashboard Bugs", :type => :feature, :sauce => false do
     expect(page).to have_content 'List item 3'
   end
 
-  # #Testing bug where you receive error message when trying to edit a provider
-  # it "- editing a provider" do
-  #   visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
-  #   within("#new_user") do
-  #     fill_in 'user_email', :with => ENV['User_Email']
-  #     fill_in 'user_password', :with => ENV['User_Password']
-  #   end
-  #   click_button 'Sign in'
-  #   expect(page).to have_content 'Signed in successfully'
-  #   click_on 'fun'
-  #   expect(page).to have_content 'Manage Content'
-  #   expect(page).to have_content 'Click the group below for which you wish to moderate.'
-  #   click_on 'Manage Content'
-  #   expect(page).to have_content 'Content Dashboard'
-  #   click_on 'Modules'
-  #   expect(page).to have_content 'Content Modules'
-  #   if page.has_text?('DO Activities')
-  #     click_on 'DO Activities'
-  #     expect(page).to have_content 'Edit Module'
-  #     click_on 'activity index provider'
-  #     expect(page).to have_content 'Content Provider'
-  #     expect(page).to have_content 'activity index provider'
-  #     click_on 'Edit'
-  #
-  #   else
-  #     find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
-  #     if page.has_text?('DO Activities')
-  #       click_on 'DO Activities'
-  #       expect(page).to have_content 'Edit Module'
-  #       click_on 'activity index provider'
-  #       expect(page).to have_content 'Content Provider'
-  #       expect(page).to have_content 'activity index provider'
-  #       click_on 'Edit'
-  #
-  #     else
-  #       find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
-  #       click_on 'DO Activities'
-  #       expect(page).to have_content 'Edit Module'
-  #       click_on 'activity index provider'
-  #       expect(page).to have_content 'Content Provider'
-  #       expect(page).to have_content 'activity index provider'
-  #       click_on 'Edit'
-  #
-  #     end
-  #   end
-  # end
+  #Testing bug where you receive error message when trying to edit a provider
+  it "- editing a provider" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
+    within("#new_user") do
+      fill_in 'user_email', :with => ENV['User_Email']
+      fill_in 'user_password', :with => ENV['User_Password']
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+    click_on 'Arm 1'
+    expect(page).to have_content 'Manage Content'
+    expect(page).to have_content 'Click the group below for which you wish to moderate.'
+    click_on 'Manage Content'
+    expect(page).to have_content 'Content Dashboard'
+    click_on 'Content Modules'
+    expect(page).to have_content 'Content Modules'
+    if page.has_text?('Testing adding/updating slides/lessons')
+      click_on 'Testing adding/updating slides/lessons'
+      expect(page).to have_content 'New Provider'
+      click_on 'slideshow provider'
+      expect(page).to have_content 'Content Provider'
+      expect(page).to have_content 'slideshow provider'
+      click_on 'Edit'
+      expect(page).to have_content 'Editing Content Provider'
+      fill_in 'content_provider_position', :with => '10'
+      click_on 'Update'
+      expect(page).to have_content 'ContentProvider was successfully updated.'
+      expect(page).to have_content 'Position: 10 / 10'
+      click_on 'Edit'
+      expect(page).to have_content 'Editing Content Provider'
+      fill_in 'content_provider_position', :with => '1'
+      click_on 'Update'
+      expect(page).to have_content 'ContentProvider was successfully updated.'
+      expect(page).to have_content 'Position: 1 / 1'
+    else
+      find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
+      if page.has_text?('Testing adding/updating slides/lessons')
+        click_on 'Testing adding/updating slides/lessons'
+        expect(page).to have_content 'New Provider'
+        click_on 'slideshow provider'
+        expect(page).to have_content 'Content Provider'
+        expect(page).to have_content 'slideshow provider'
+        click_on 'Edit'
+        expect(page).to have_content 'Editing Content Provider'
+        fill_in 'content_provider_position', :with => '10'
+        click_on 'Update'
+        expect(page).to have_content 'ContentProvider was successfully updated.'
+        expect(page).to have_content 'Position: 10 / 10'
+        click_on 'Edit'
+        expect(page).to have_content 'Editing Content Provider'
+        fill_in 'content_provider_position', :with => '1'
+        click_on 'Update'
+        expect(page).to have_content 'ContentProvider was successfully updated.'
+        expect(page).to have_content 'Position: 1 / 1'
+      else
+        find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
+          click_on 'Testing adding/updating slides/lessons'
+          expect(page).to have_content 'New Provider'
+          click_on 'slideshow provider'
+          expect(page).to have_content 'Content Provider'
+          expect(page).to have_content 'slideshow provider'
+          click_on 'Edit'
+          expect(page).to have_content 'Editing Content Provider'
+          fill_in 'content_provider_position', :with => '10'
+          click_on 'Update'
+          expect(page).to have_content 'ContentProvider was successfully updated.'
+          expect(page).to have_content 'Position: 10 / 10'
+          click_on 'Edit'
+          expect(page).to have_content 'Editing Content Provider'
+          fill_in 'content_provider_position', :with => '1'
+          click_on 'Update'
+          expect(page).to have_content 'ContentProvider was successfully updated.'
+          expect(page).to have_content 'Position: 1 / 1'
+      end
+    end
+  end
 end
