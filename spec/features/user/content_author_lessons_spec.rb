d8d6953a79bbaa477a28ffab2e357ca34c0bc8ba
Christopher Brenner <christopher.brenner1@northwestern.edu>
2014-12-14 18:13:34 -0600

    update content author lessons spec

--- spec/features/user/content_author_lessons_spec.rb
+++ spec/features/user/content_author_lessons_spec.rb
@@ -1,6 +1,6 @@
 #filename: content_author_lessons_spec.rb
 
-#this is to test the users functionality on the researcher dashboard.
+#this is to test the users Arm 1ctionality on the researcher dashboard.
 
 require_relative '../../../spec/spec_helper'
 require_relative '../../../spec/configure_cloud_saucelabs'
@@ -8,17 +8,16 @@ require_relative '../../../spec/configure_cloud_saucelabs'
 
 
 #to run locally comment this line out
-# describe "Content Author, Lessons", :type => :feature, :sauce => true do
+# describe "Content Author, Lesson Modules", :type => :feature, :sauce => true do
 
 #to run on Sauce Labs comment this block out
-describe "Content Author, Lessons", :type => :feature, :sauce => false do
+describe "Content Author, Lesson Modules", :type => :feature, :sauce => false do
 
   before(:each) do
     Capybara.default_driver = :selenium
   end
 
 #tests
-
   #Testing creating a lesson
   it "- new lesson" do
     visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
@@ -28,13 +27,13 @@ describe "Content Author, Lessons", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Lessons'
-    expect(page).to have_content 'Listing Lessons'
+    click_on 'Lesson Modules'
+    expect(page).to have_content 'Listing Lesson Modules'
     click_on 'New'
     fill_in 'lesson_title', :with => 'Test lesson'
     fill_in 'lesson_position', :with => '45'
@@ -53,23 +52,23 @@ describe "Content Author, Lessons", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Lessons'
-    expect(page).to have_content 'Listing Lessons'
+    click_on 'Lesson Modules'
+    expect(page).to have_content 'Listing Lesson Modules'
     click_on 'Testing adding/updating slides/lessons'
     expect(page).to have_content 'Test 1'
     expect(page).to have_content 'Test video 1'
-    find(:xpath, 'html/body/div[1]/div/div/div[2]/div[1]/a[4]').click
+    find(:xpath, 'html/body/div[1]/div/div/div[2]/div[1]/a[2]').click
     fill_in 'lesson_title', :with => 'Testing adding/updating slides/lessons 123'
     click_on 'Update'
     expect(page).to have_content 'Successfully updated lesson'
     expect(page).to have_content 'Testing adding/updating slides/lessons 123'
     expect(page).to have_content 'Add Video Slide'
-    find(:xpath, 'html/body/div[1]/div/div/div[2]/div[1]/a[4]').click
+    find(:xpath, 'html/body/div[1]/div/div/div[2]/div[1]/a[2]').click
     fill_in 'lesson_title', :with => 'Testing adding/updating slides/lessons'
     click_on 'Update'
     expect(page).to have_content 'Successfully updated lesson'
@@ -86,13 +85,13 @@ describe "Content Author, Lessons", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Lessons'
-    expect(page).to have_content 'Listing Lessons'
+    click_on 'Lesson Modules'
+    expect(page).to have_content 'Listing Lesson Modules'
     source = page.find(:xpath, 'html/body/div[1]/div/div/div[2]/table/tbody/tr[42]/td[1]/span/i')
     target = page.find(:xpath, 'html/body/div[1]/div/div/div[2]/table/tbody/tr[40]/td[1]/span/i')
     source.drag_to(target)
@@ -122,14 +121,14 @@ describe "Content Author, Lessons", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Lessons'
-    expect(page).to have_content 'Listing Lessons'
-    find(:xpath, 'html/body/div[1]/div/div/div[2]/table/tbody/tr[44]/td[6]/a[2]').click
+    click_on 'Lesson Modules'
+    expect(page).to have_content 'Listing Lesson Modules'
+    find(:xpath, 'html/body/div[1]/div/div/div[2]/table/tbody/tr[44]/td[5]/a[2]').click
     page.accept_alert 'Are you sure?'
     expect(page).to have_content 'Lesson deleted'
     expect(page).to_not have_content 'Test lesson'
