7a30be0a113dcc9116e0a145eed11f35dd96b4a7
Christopher Brenner <christopher.brenner1@northwestern.edu>
2014-12-14 19:06:30 -0600

    updated content author modules spec

--- spec/features/user/content_author_modules_spec.rb
+++ spec/features/user/content_author_modules_spec.rb
@@ -1,6 +1,6 @@
 #filename: content_author_modules_spec.rb
 
-#this is to test the users functionality on the researcher dashboard.
+#this is to test the users Arm 1ctionality on the researcher dashboard.
 
 require_relative '../../../spec/spec_helper'
 require_relative '../../../spec/configure_cloud_saucelabs'
@@ -26,14 +26,14 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Modules'
+    click_on 'Content Modules'
     expect(page).to have_content 'Content Modules'
-    click_on 'New Module'
+    click_on 'New'
     expect(page).to have_content 'New Content Module'
     fill_in 'content_module_title', :with => 'Test content module'
     select 'THINK', :from => 'content_module_bit_core_tool_id'
@@ -52,22 +52,22 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Modules'
+    click_on 'Content Modules'
     expect(page).to have_content 'Content Modules'
     if page.has_text?("#1 Awareness")
       click_on '#1 Awareness'
-      click_on 'Edit Module'
+      click_on 'Edit'
       select 'THINK', :from => 'content_module_bit_core_tool_id'
       fill_in 'content_module_position', :with => '9'
       click_on 'Update'
       expect(page).to have_content 'Content module was successfully updated.'
       expect(page).to have_content 'Tool: THINK'
-      click_on 'Edit Module'
+      click_on 'Edit'
       select 'DO', :from => 'content_module_bit_core_tool_id'
       fill_in 'content_module_position', :with => '2'
       click_on 'Update'
@@ -77,13 +77,13 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
       find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
       if page.has_text?("#1 Awareness")
         click_on '#1 Awareness'
-        click_on 'Edit Module'
+        click_on 'Edit'
         select 'THINK', :from => 'content_module_bit_core_tool_id'
         fill_in 'content_module_position', :with => '9'
         click_on 'Update'
         expect(page).to have_content 'Content module was successfully updated.'
         expect(page).to have_content 'Tool: THINK'
-        click_on 'Edit Module'
+        click_on 'Edit'
         select 'DO', :from => 'content_module_bit_core_tool_id'
         fill_in 'content_module_position', :with => '2'
         click_on 'Update'
@@ -92,13 +92,13 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
       else
         find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
         click_on '#1 Awareness'
-        click_on 'Edit Module'
+        click_on 'Edit'
         select 'THINK', :from => 'content_module_bit_core_tool_id'
         fill_in 'content_module_position', :with => '9'
         click_on 'Update'
         expect(page).to have_content 'Content module was successfully updated.'
         expect(page).to have_content 'Tool: THINK'
-        click_on 'Edit Module'
+        click_on 'Edit'
         select 'DO', :from => 'content_module_bit_core_tool_id'
         fill_in 'content_module_position', :with => '2'
         click_on 'Update'
@@ -117,12 +117,12 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Modules'
+    click_on 'Content Modules'
     expect(page).to have_content 'Content Modules'
     if page.has_text?("Test content module")
       click_on 'Test content module'
@@ -158,77 +158,107 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Modules'
+    click_on 'Content Modules'
     expect(page).to have_content 'Content Modules'
     click_on 'New Provider'
     expect(page).to have_content 'New Content Provider'
-    select 'DO: DO Activities', :from => 'content_provider_bit_core_content_module_id'
+    select 'LEARN: Testing adding/updating slides/lessons', :from => 'content_provider_bit_core_content_module_id'
     select 'slideshow provider', :from => 'content_provider_type'
     select 'BitCore::Slideshow', :from => 'content_provider_source_content_type'
     select 'Home Introduction', :from => 'content_provider_source_content_id'
-    fill_in 'content_provider_position', :with => '2'
+    fill_in 'content_provider_position', :with => '4'
     check 'content_provider_show_next_nav'
     check 'content_provider_is_skippable_after_first_viewing'
     click_on 'Create'
     expect(page).to have_content 'ContentProvider was successfully created.'
-    expect(page).to have_content 'Tool: DO'
-    expect(page).to have_content 'Module: DO Activities'
-    expect(page).to have_content 'Position: 2 / 2'
+    expect(page).to have_content 'Tool: LEARN'
+    expect(page).to have_content 'Module: Testing adding/updating slides/lessons'
+    expect(page).to have_content 'Position: 4 / 4'
     expect(page).to have_content 'Is skippable after first viewing: true'
     expect(page).to have_content 'Slideshow: Home Introduction'
   end
 
-  # #Testing updating a provider
-  # it "- destroying a provider" do
-  #   visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
-  #   within("#new_user") do
-  #     fill_in 'user_email', :with => ENV['User_Email']
-  #     fill_in 'user_password', :with => ENV['User_Password']
-  #   end
-  #   click_button 'Sign in'
-  #   expect(page).to have_content 'Signed in successfully'
-  #   click_on 'fun'
-  #   expect(page).to have_content 'Manage Content'
-  #   expect(page).to have_content 'Click the group below for which you wish to moderate.'
-  #   click_on 'Manage Content'
-  #   expect(page).to have_content 'Content Dashboard'
-  #   click_on 'Modules'
-  #   expect(page).to have_content 'Content Modules'
-  #   if page.has_text?('DO Activities')
-  #     click_on 'DO Activities'
-  #     expect(page).to have_content 'Edit Module'
-  #     click_on 'activity index provider'
-  #     expect(page).to have_content 'Content Provider'
-  #     expect(page).to have_content 'activity index provider'
-  #     click_on 'Edit'
-  #
-  #   else
-  #     find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
-  #     if page.has_text?('DO Activities')
-  #       click_on 'DO Activities'
-  #       expect(page).to have_content 'Edit Module'
-  #       click_on 'activity index provider'
-  #       expect(page).to have_content 'Content Provider'
-  #       expect(page).to have_content 'activity index provider'
-  #       click_on 'Edit'
-  #
-  #     else
-  #       find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
-  #       click_on 'DO Activities'
-  #       expect(page).to have_content 'Edit Module'
-  #       click_on 'activity index provider'
-  #       expect(page).to have_content 'Content Provider'
-  #       expect(page).to have_content 'activity index provider'
-  #       click_on 'Edit'
-  #
-  #     end
-  #   end
-  # end
+  #Testing updating a provider
+  it "- updating a provider" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
+    within("#new_user") do
+      fill_in 'user_email', :with => ENV['User_Email']
+      fill_in 'user_password', :with => ENV['User_Password']
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+    click_on 'Arm 1'
+    expect(page).to have_content 'Manage Content'
+    expect(page).to have_content 'Click the group below for which you wish to moderate.'
+    click_on 'Manage Content'
+    expect(page).to have_content 'Content Dashboard'
+    click_on 'Content Modules'
+    expect(page).to have_content 'Content Modules'
+    if page.has_text?('Testing adding/updating slides/lessons')
+      click_on 'Testing adding/updating slides/lessons'
+      expect(page).to have_content 'New Provider'
+      click_on '1 slideshow provider'
+      expect(page).to have_content 'Content Provider'
+      expect(page).to have_content 'slideshow provider'
+      click_on 'Edit'
+      expect(page).to have_content 'Editing'
+      fill_in 'content_provider_position', :with => '10'
+      click_on 'Update'
+      expect(page).to have_content 'ContentProvider was successfully updated.'
+      expect(page).to have_content 'Position: 10 / 10'
+      click_on 'Edit'
+      expect(page).to have_content 'Editing'
+      fill_in 'content_provider_position', :with => '1'
+      click_on 'Update'
+      expect(page).to have_content 'ContentProvider was successfully updated.'
+      expect(page).to have_content 'Position: 1 / 4'
+    else
+      find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
+      if page.has_text?('Testing adding/updating slides/lessons')
+        click_on 'Testing adding/updating slides/lessons'
+        expect(page).to have_content 'New Provider'
+        click_on '1 slideshow provider'
+        expect(page).to have_content 'Content Provider'
+        expect(page).to have_content 'slideshow provider'
+        click_on 'Edit'
+        expect(page).to have_content 'Editing'
+        fill_in 'content_provider_position', :with => '10'
+        click_on 'Update'
+        expect(page).to have_content 'ContentProvider was successfully updated.'
+        expect(page).to have_content 'Position: 10 / 10'
+        click_on 'Edit'
+        expect(page).to have_content 'Editing'
+        fill_in 'content_provider_position', :with => '1'
+        click_on 'Update'
+        expect(page).to have_content 'ContentProvider was successfully updated.'
+        expect(page).to have_content 'Position: 1 / 4'
+      else
+        find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
+        click_on 'Testing adding/updating slides/lessons'
+        expect(page).to have_content 'New Provider'
+        click_on '1 slideshow provider'
+        expect(page).to have_content 'Content Provider'
+        expect(page).to have_content 'slideshow provider'
+        click_on 'Edit'
+        expect(page).to have_content 'Editing'
+        fill_in 'content_provider_position', :with => '10'
+        click_on 'Update'
+        expect(page).to have_content 'ContentProvider was successfully updated.'
+        expect(page).to have_content 'Position: 10 / 10'
+        click_on 'Edit'
+        expect(page).to have_content 'Editing'
+        fill_in 'content_provider_position', :with => '1'
+        click_on 'Update'
+        expect(page).to have_content 'ContentProvider was successfully updated.'
+        expect(page).to have_content 'Position: 1 / 4'
+      end
+    end
+  end
 
   #Testing destroying a provider
   it "- destroying a provider" do
@@ -239,17 +269,17 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     end
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
-    click_on 'fun'
+    click_on 'Arm 1'
     expect(page).to have_content 'Manage Content'
     expect(page).to have_content 'Click the group below for which you wish to moderate.'
     click_on 'Manage Content'
     expect(page).to have_content 'Content Dashboard'
-    click_on 'Modules'
+    click_on 'Content Modules'
     expect(page).to have_content 'Content Modules'
-    if page.has_text?('DO Activities')
-      click_on 'DO Activities'
-      expect(page).to have_content 'Edit Module'
-      click_on 'slideshow provider'
+    if page.has_text?('Testing adding/updating slides/lessons')
+      click_on 'Testing adding/updating slides/lessons'
+      expect(page).to have_content 'Edit'
+      click_on '4 slideshow provider'
       expect(page).to have_content 'Slideshow: Home Introduction'
       click_on 'Destroy'
       page.accept_alert 'Are you sure?'
@@ -257,18 +287,18 @@ describe "Content Author, Modules", :type => :feature, :sauce => false do
     else
       find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[3]').click
       if page.has_text?('DO Activities')
-        click_on 'DO Activities'
-        expect(page).to have_content 'Edit Module'
-        click_on 'slideshow provider'
+        click_on 'Testing adding/updating slides/lessons'
+        expect(page).to have_content 'Edit'
+        click_on '4 slideshow provider'
         expect(page).to have_content 'Slideshow: Home Introduction'
         click_on 'Destroy'
         page.accept_alert 'Are you sure?'
         expect(page).to have_content 'Content provider was successfully destroyed.'
       else
         find(:xpath, 'html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div/ul/li[4]').click
-        click_on 'DO Activities'
-        expect(page).to have_content 'Edit Module'
-        click_on 'slideshow provider'
+        click_on 'Testing adding/updating slides/lessons'
+        expect(page).to have_content 'Edit'
+        click_on '4 slideshow provider'
         expect(page).to have_content 'Slideshow: Home Introduction'
         click_on 'Destroy'
         page.accept_alert 'Are you sure?'
