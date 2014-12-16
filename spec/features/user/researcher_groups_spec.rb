e89911a15e0f228a9438f6d6d7813fc8c6ca3595
Christopher Brenner <christopher.brenner1@northwestern.edu>
2014-12-16 10:10:13 -0600

    finished researcher groups spec by adding a test for updating moderator for a group.

--- spec/features/user/researcher_groups_spec.rb
+++ spec/features/user/researcher_groups_spec.rb
@@ -52,20 +52,45 @@ describe "Research, Groups", :type => :feature, :sauce => false do
     expect(page).to have_content 'CSV Reports'
     click_on 'Groups'
     expect(page).to have_content 'Listing Groups'
-    click_on 'fun'
-    expect(page).to have_content 'Title: fun'
+    click_on 'fake'
+    expect(page).to have_content 'Title: fake'
     click_on 'Edit'
     expect(page).to have_content 'Editing Group'
-    fill_in 'group_title', :with => 'Updated fun'
+    fill_in 'group_title', :with => 'Updated fake'
     click_on 'Update'
     expect(page).to have_content 'Group was successfully updated.'
-    expect(page).to have_content 'Title: Updated fun'
+    expect(page).to have_content 'Title: Updated fake'
     click_on 'Edit'
     expect(page).to have_content 'Editing Group'
-    fill_in 'group_title', :with => 'fun'
+    fill_in 'group_title', :with => 'fake'
     click_on 'Update'
     expect(page).to have_content 'Group was successfully updated.'
-    expect(page).to have_content 'Title: fun'
+    expect(page).to have_content 'Title: fake'
+  end
+
+  #Testing adding/removing a moderator from a group
+  it "- update a group" do
+    visit 'https://steppedcare-staging.cbits.northwestern.edu/users/sign_in'
+    within("#new_user") do
+      fill_in 'user_email', :with => ENV['User_Email']
+      fill_in 'user_password', :with => ENV['User_Password']
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+    click_on 'Researcher Dashboard'
+    expect(page).to have_content 'CSV Reports'
+    click_on 'Groups'
+    expect(page).to have_content 'Listing Groups'
+    click_on 'fake'
+    expect(page).to have_content 'Title: fake'
+    click_on 'Remove Moderator'
+    expect(page).to have_content 'Moderator was successfully removed.'
+    expect(page).to have_content 'Moderator: None'
+    click_on 'Edit'
+    select ENV['Clinician_Email'], :from => 'group_user_id'
+    click_on 'Update'
+    expect(page).to have_content 'Groups was successfully updated.'
+    expect(page).to have_content 'Moderator: ' + ENV['Clinician_Email']
   end
 
   #Testing destroying a group
