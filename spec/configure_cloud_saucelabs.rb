4cdfb3f0d8cf5307c995f47463f161a4a78667d2
Christopher Brenner <christopher.brenner1@northwestern.edu>
2014-12-16 14:06:13 -0600

    Update configure_cloud_saucelabs.rb

--- spec/configure_cloud_saucelabs.rb
+++ spec/configure_cloud_saucelabs.rb
@@ -1,7 +1,5 @@
 # filename: configure_cloud_saucelabs.rb
 
-whatever!
-
 ENV['SAUCE_USERNAME']           ||= 'Your Sauce Username'
 ENV['SAUCE_ACCESS_KEY']         ||= 'Your Sauce Access Key'
 ENV['Participant_Email']        ||= 'SC Participant Email'
